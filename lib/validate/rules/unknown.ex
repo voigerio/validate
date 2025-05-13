6f1a2a7f1e26572859ac3a1d098220ae9cc1e18c
rakesh-voiger <rakesh@voiger.io>
2025-05-13 23:45:02 +0400

    Update unknown.ex

--- lib/validate/rules/unknown.ex
+++ lib/validate/rules/unknown.ex
@@ -3,6 +3,12 @@ defmodule Validate.Rules.Unknown do
 
   import Validate.Validator
 
+  # Dummy one to not panic with unknown validator not present.
+  # We cannot fully rely upon this style like other validators
+  # as the map value gets modified in cases like unknown: allow
+  # which distracts the other validators.
+  # Hence an explcit validator added on the validator.ex to handle
+  # at the end of the map validation
   def validate(%{value: value}) do
     success(value)
   end
